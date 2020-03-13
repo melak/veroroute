8923f1704def99bd85cdc01acb1d2c5000ef8723
dralx <dralx@d5f823ff-b8c7-416f-ab5d-d69718ecf442>
2020-03-13 23:23:24 +0000

    Added manpage
    git-svn-id: https://svn.code.sf.net/p/veroroute/code/trunk@157 d5f823ff-b8c7-416f-ab5d-d69718ecf442

--- src/main.cpp
+++ src/main.cpp
@@ -23,18 +23,15 @@
 
 void versionmsg()
 {
-	std::cout << "VeroRoute V" << szVEROROUTE_VERSION << std::endl;
+	std::cout << "VeroRoute Version " << szVEROROUTE_VERSION << std::endl;
 }
 
 void helpmsg()
 {
-	std::cout << "Usage: veroroute" << std::endl;
-	std::cout << "       veroroute [OPTION]" << std::endl;
-	std::cout << "       veroroute [FILE] [OPTION]"<< std::endl;
-	std::cout << "       veroroute [OPTION] [FILE]"<< std::endl << std::endl;
-	std::cout << "Starts veroroute, and opens the specified VRT file." << std::endl << std::endl;
+	std::cout << "Usage: veroroute [OPTION] [FILE]"<< std::endl << std::endl;
+	std::cout << "Starts the application and opens the specified FILE (a VeroRoute VRT file)." << std::endl << std::endl;
 	std::cout << "[OPTION] is one of the following" << std::endl;
-	std::cout << "   -p [PATH]       specify the path to the tutorials directory" << std::endl;
+	std::cout << "   -p PATH         specify the PATH to the 'tutorials' directory" << std::endl;
 	std::cout << "   -h, --help      display this help and exit" << std::endl;
 	std::cout << "   -v, --version   display version information and exit" << std::endl;
 }
