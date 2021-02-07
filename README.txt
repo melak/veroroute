9ddd59c7ab1b549cbd9f89f53b2a22940c5dbe3e
dralx <dralx@d5f823ff-b8c7-416f-ab5d-d69718ecf442>
2021-02-07 20:14:20 +0000

    V2.03 Mark up
    git-svn-id: https://svn.code.sf.net/p/veroroute/code/trunk@297 d5f823ff-b8c7-416f-ab5d-d69718ecf442

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 VeroRoute - Qt based Veroboard/Perfboard/PCB layout & routing application.
 
-Version 2.02
+Version 2.03
 
 Copyright (C) 2017  Alex Lawrow    ( dralx@users.sourceforge.net )
 
--- src/Version.h
+++ src/Version.h
@@ -261,7 +261,7 @@ static const char* szVEROROUTE_VERSION =
 			// Added ability to define custom pad/hole size in component editor.
 			// Added ability to add veroboard labels (numbers or letters).
 			// Improved algorithm for solder bridges.  Don't show a solder bridge if a wire is already doing that job.
-  "2.02";	// Bug fix: Fill slider should be enabled when track slider is set to Off.
+//"2.02";	// Bug fix: Fill slider should be enabled when track slider is set to Off.
 			// Bug fix: Calculation of minimum track separation was not correct when using fat tracks style.
 			// Bug fix: Changed render order in Color/Mono mode to fix issues with how custom sizes pads are drawn.
 			// Track width changes:  Minimum allowed track widths lowered to 4 mil.
@@ -270,4 +270,7 @@ static const char* szVEROROUTE_VERSION =
 			//                       Maximum custom pad size increased to 130 mil.
 			//                       Maximum custom hole size increased to 100 mil.
 			// New feature:  Add ability to show locations of minimum track separation.
+			// Updated tutorials.
+  "2.03";	// Bug fix: Wire rendering broken in 2.02.
+			// New feature. Can specify layer preference on component pins of 2-layer PCBs.
 			// Updated tutorials.
