9ab62fe1938328130eb6d02af00be79552f57d3e
dralx <dralx@d5f823ff-b8c7-416f-ab5d-d69718ecf442>
2022-04-27 08:19:19 +0000

    V2.25 Mark up
    git-svn-id: https://svn.code.sf.net/p/veroroute/code/trunk@493 d5f823ff-b8c7-416f-ab5d-d69718ecf442

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 VeroRoute - Qt based Veroboard/Perfboard/PCB layout & routing application.
 
-Version 2.24
+Version 2.25
 
 Copyright (C) 2017-2022  Alex Lawrow    ( dralx@users.sourceforge.net )
 
--- src/Version.h
+++ src/Version.h
@@ -2,7 +2,10 @@
 
 static const char* szVEROROUTE_VERSION =
 
-"2.24";		// Bug fix: Broken Nets list was not updating the main view if user scrolled to a list item instead of clicking on it.
+"2.25";		// Bug fix: Major bug.  Double-clicking on a user-defined template to add the part to the layout
+			//			can give the wrong footprint unless the part was created using component editor.
+			// Bug fix: Some actions in the Key/Mouse Actions Dialog were removed in V2.20 but still listed.
+//"2.24";	// Bug fix: Broken Nets list was not updating the main view if user scrolled to a list item instead of clicking on it.
 //"2.23";	// New feature: Option to force thermal reliefs to use an X-shaped pattern of tags.
 			// Speeded-up code for spanning-trees and connection matrices.
 //"2.22";	// Bug fix: Fix extent of protrusion into ground-fill by extra thermal reliefs.
