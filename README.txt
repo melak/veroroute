43da3f04d78a9ea28d06b94ae260a4b8b2702e6e
dralx <dralx@d5f823ff-b8c7-416f-ab5d-d69718ecf442>
2022-05-31 20:39:07 +0000

    V2.28 Mark up
    git-svn-id: https://svn.code.sf.net/p/veroroute/code/trunk@500 d5f823ff-b8c7-416f-ab5d-d69718ecf442

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 VeroRoute - Qt based Veroboard/Perfboard/PCB layout & routing application.
 
-Version 2.27
+Version 2.28
 
 Copyright (C) 2017-2022  Alex Lawrow    ( dralx@users.sourceforge.net )
 
--- src/Version.h
+++ src/Version.h
@@ -2,7 +2,8 @@
 
 static const char* szVEROROUTE_VERSION =
 
-"2.27";		// Bug fix: Bug in component manager leads to incorrect warning about circuit not being saved (even if it has been).
+"2.28";		// Bug fix: Code for X-thermals not properly handling isolated ground pad on 2-layer board.
+//"2.27";	// Bug fix: Bug in component manager leads to incorrect warning about circuit not being saved (even if it has been).
 			// Disabled use of double-click for rotating parts.
 //"2.26";	// Removed infrequently used icons from the toolbar.  Added tutorial info on text boxes.
 //"2.25";	// Bug fix: Major bug.  Double-clicking on a user-defined template to add the part to the layout
