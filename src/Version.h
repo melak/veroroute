ef10f870d29a5168bfdf8c3e814a4b2fe04b7c29
dralx <dralx@d5f823ff-b8c7-416f-ab5d-d69718ecf442>
2023-05-18 18:27:30 +0000

    Fix bad check-in
    git-svn-id: https://svn.code.sf.net/p/veroroute/code/trunk@635 d5f823ff-b8c7-416f-ab5d-d69718ecf442

--- src/Version.h
+++ src/Version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 static const char* szVEROROUTE_VERSION =
-"2.33"		// Bug fix: Label offsets were not persisted by the Component Editor.
+"2.33";		// Bug fix: Label offsets were not persisted by the Component Editor.
 			// Bug fix: If not forcing X-thermals, the code for producing additional thermal relief tags was missing extra checks for tags in the diagonal direction.
 			// Bug fix: File -> Open (merge into current) could show the newly loaded board with PCB style shapes while in Color mode.
 			// Bug fix: Autocrop and Margin controls were not immediately updating the board size info in the status bar.
